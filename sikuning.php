b3799e61b59d4422b3cd88c0a64c496fa773617f
andarualwan <43308131+andarualwan@users.noreply.github.com>
2018-09-17 22:37:19 +0700

    github

---
+++ sikuning.php
@@ -0,0 +1,9 @@
+<b>KATANYA</b> kalau beli paket yang 1GB Rp 1,5k lalu di timpa paket UNLI 7HR Rp 0 bisa jadi UNLIMITED no FUP<hr>
+<form action="sikuning.php" method="POST">
+    <input type="text" name="hp" placeholder="6285xx or 085xx" /><br/>
+    <select name="beli" style="width:147px">
+<option value="free">UNLI 7HR Rp 0</option>
+<option value="gak">1GB 1HR Rp 1,5K</option>
+</select><br/>
+    <input type="submit" value="GAS !">
+</form>[IKLAN] Cukup donasi saya dengan klik <a href="http://adserver.adreactor.com/servlet/view/window/url/zone?zid=40&pid=4947"> Sini </a> aja kak :(
