0d1263ff7ca6e1a4e3dd9b7f8601f17ced18dbbe
bonangjaya <talantulla@gmail.com>
2018-10-19 15:10:47 +0700

    Update sikuning.php

--- sikuning.php
+++ sikuning.php
@@ -1,9 +1,8 @@
-<b>KATANYA</b> kalau beli paket yang 1GB Rp 1,5k lalu di timpa paket UNLI 7HR Rp 0 bisa jadi UNLIMITED no FUP<hr>
+<b>KATANYA</b> tidak bisa sukur<hr>
 <form action="sikuning.php" method="POST">
     <input type="text" name="hp" placeholder="6285xx or 085xx" /><br/>
     <select name="beli" style="width:147px">
 <option value="free">UNLI 7HR Rp 0</option>
 <option value="gak">1GB 1HR Rp 1,5K</option>
 </select><br/>
-    <input type="submit" value="GAS !">
-</form>[IKLAN] Cukup donasi saya dengan klik <a href="http://adserver.adreactor.com/servlet/view/window/url/zone?zid=40&pid=4947"> Sini </a> aja kak :(
+    <input type="submit" value="yes !">
